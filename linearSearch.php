213e92dfd51bc0a6714a06587f7f645ad7797779
MogahidGaffar <mogahidgaffar@gmail.com>
2023-12-12 18:30:33 -0800

    add comment to the  linear search code

--- linearSearch.php
+++ linearSearch.php
@@ -1,32 +1,17 @@
 <?php
-
-function linearsearch ($arr , $element){
-
-
-    $n = count ($arr);
-    $key= $element;
-
-    for ($i = 0 ; $i < $n;$i++){
-
-        if($arr[$i] == $key){
-            return "the Element is found in the index : ".$i;
+function linearsearch ($arr , $element){                        //declaring the function
+$n = count ($arr);                                              //getting the size of the array
+    $key= $element;                                             //getting the size of the array
+    for ($i = 0 ; $i < $n;$i++){                                //for loop in the array
+       if($arr[$i] == $key){                                    //check if the elment is exists
+            return "the Element is found in the index : ".$i;   //print message  if the elment is exists
         }
-
     }
-
-    return "this  Element is not exists";
-
-
+    return "this  Element is not exists";                       //print message if the elment is not exists
 }
+$array =[11,9,5,21];                                            //declaring an array of elements
+$element =5;                                                    //declaring variable containg the required element 
 
-$array =[11,9,5,21];
-
-$element =5;
-
-echo linearsearch($array,$element);
-
-
-
-
+echo linearsearch($array,$element);                           //calling the function
 
 ?>
